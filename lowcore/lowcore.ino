62723d21e321c5ef32beada2790aaa551543b86e
Masafumim Horimoto on smena8 <masafumi.horimoto@gmail.com>
2021-03-27 02:09:19 +0900

    feat: eeprom初期値編集プログラム lowcore 完成

--- lowcore/lowcore.ino
+++ lowcore/lowcore.ino
@@ -11,16 +11,44 @@
 #include <EEPROM.h>
 
 void setup(void) {
-  unsigned int i,j,r;
+  unsigned int i,j,r,a;
   byte t;
-  char c,name[8];
-  byte uecsid[6]={0x10,0x10,0x0c,0x00,0x00,0x08};
+  char c,name[8],z[4];
+  byte uecsid[6]={0x10,0x10,0x0c,0x00,0x00,0x08},uecsrd[6];
   byte macadd[6]={0x02,0xa2,0x73,0x08,0xff,0xff};
   byte data[16] ={'K',1,1,1,0,15,10,0,'T','C','T','e','m','p',0,0};
 
   Serial.begin(115200);
   Serial.println("EEPROM SETTING");
-  //EEPROM.put(0x0,uecsid);
+  EEPROM.get(0x0,uecsrd);
+  for(i=0;i<6;i++) {
+    if (uecsrd[i]!=uecsid[i]) {
+      r = 1;
+      break;
+    } else {
+      r = 0;
+    }
+  }
+  if (r==0) {
+    Serial.println("FINE DID");
+    Serial.println("HEXDATA:");
+    for(j=0;j<9;j++) {
+      sprintf(z,"0x%03X:",j*0x10);
+      Serial.print(z);
+      for(i=0;i<16;i++) {
+	sprintf(z,"%02X",EEPROM.read(i+(j*0x10)));
+	Serial.print(z);
+	if (i<15) Serial.print(",");
+      }
+      Serial.println();
+    }
+    Serial.end();
+    while(1) {
+      r = 1;
+      r = 2;
+    }
+  }
+  EEPROM.put(0x0,uecsid);
   Serial.println("WRITE DONE");
   Serial.print("UECSID:");
   for(i=0;i<6;i++) {
@@ -53,7 +81,7 @@ void setup(void) {
     if (i<5) Serial.print(":");
   }
   Serial.println(); 
-  //EEPROM.put(0x6,macadd);
+  EEPROM.put(0x6,macadd);
   Serial.print("MAC=");
   for(i=0;i<6;i++) {
     Serial.print(EEPROM.read(6+i),HEX);
@@ -61,12 +89,16 @@ void setup(void) {
   }
   Serial.println("");
   for(i=0;i<8;i++) {
-    //EEPROM.put((i*0x10)+0x10,data);
+    a = (i*0x10)+0x10;
+    Serial.print("0x");
+    Serial.print(a,HEX);
+    EEPROM.put(a,data);
+    Serial.println(" WROTE");
   }
   Serial.println("HEXDATA:");
   for(j=0;j<9;j++) {
     for(i=0;i<16;i++) {
-      Serial.print(EEPROM.read(i),HEX);
+      Serial.print(EEPROM.read(i+(j*0x10)),HEX);
       if (i<15) Serial.print(",");
     }
     Serial.println();
